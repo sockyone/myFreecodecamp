43939fa213be7c3a64a6fa5a0a488569f66ee958
sockyone <35925547+sockyone@users.noreply.github.com>
2018-10-10 21:11:24 +0700

    Create server.js

---
+++ server.js
@@ -0,0 +1,50 @@
+// server.js
+// where your node app starts
+
+// init project
+var express = require('express');
+var app = express();
+var moment = require('moment');
+
+// enable CORS (https://en.wikipedia.org/wiki/Cross-origin_resource_sharing)
+// so that your API is remotely testable by FCC 
+var cors = require('cors');
+app.use(cors({optionSuccessStatus: 200}));  // some legacy browsers choke on 204
+
+// http://expressjs.com/en/starter/static-files.html
+app.use(express.static('public'));
+
+// http://expressjs.com/en/starter/basic-routing.html
+app.get("/", function (req, res) {
+  res.sendFile(__dirname + '/views/index.html');
+});
+
+
+// your first API endpoint... 
+app.get("/api/hello", function (req, res) {
+  res.json({greeting: 'hello API'});
+});
+
+app.get("/api/timestamp",function(req,res){
+  var date = new Date();
+  res.json({"unix":date.getTime(),"utc":date.toUTCString()});
+});
+
+app.get("/api/timestamp/:time",function(req,res){
+  
+  if (moment(req.params.time).isValid()) {
+    var date = new Date(req.params.time);
+    res.json({"unix":date.getTime(),"utc":date.toUTCString()});
+  }
+    
+  else {
+    res.json({"error" : "Invalid Date" });
+  }
+});
+
+
+
+// listen for requests :)
+var listener = app.listen(process.env.PORT, function () {
+  console.log('Your app is listening on port ' + listener.address().port);
+});
